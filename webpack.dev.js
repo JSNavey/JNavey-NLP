d60ee60d06e75863aa3ae80fcbf7fbf9ed3a8a88
Ja Navey <jsnavey@gmail.com>
2023-07-17 00:35:28 -0400

    feat: Convert css files to scss

--- webpack.dev.js
+++ webpack.dev.js
@@ -13,6 +13,10 @@ module.exports = {
                     test: /\.js$/,
                     exclude: /node_modules/,
                     loader: "babel-loader"
+                },
+                {
+                    test: /\.scss$/,
+                    use: [ "style-loader", "css-loaedr", "sass-loader"]
                 }
             ]
     },
